13183109153ce630f3719dcb185faa46a50398c4
ahmadalashtar01 <41813167+ahmadalashtar01@users.noreply.github.com>
2021-12-31 12:40:56 +0300

    Add files via upload

---
+++ project_guessing_game.rb
@@ -0,0 +1,29 @@
+class GuessingGame
+	def initialize(min,max)
+		@secret_num = rand(min...max)
+		@num_attempts = 0
+		@game_over = false
+	end
+	def num_attempts
+		return @num_attempts
+	end
+	def game_over?
+		return @game_over
+	end
+	def check_num(num)
+		@num_attempts += 1
+		if @secret_num == num
+			@game_over = true
+			puts "you win"
+		elsif num > @secret_num
+			puts "too big"
+		else
+			puts "too small"
+		end
+	end
+	def ask_user
+		puts "enter a number"
+		n = gets.chomp.to_i
+		check_num(n)
+	end
+end
